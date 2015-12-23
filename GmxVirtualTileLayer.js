d4854d57e4f0f69b8c14b0d2ca7ffd6cfd3f2680
Alexander Parshin <parshin.alexander@gmail.com>
2015-12-23 11:59:26 +0300

    Added new component GmxVirtualTileLayer

---
+++ GmxVirtualTileLayer.js
@@ -0,0 +1,25 @@
+﻿/** GeoMixer virtual layer for standard tile raster layers (L.TileLayer)
+*/
+(function (){
+
+'use strict';
+
+var GmxVirtualTileLayer = function(options) {}
+
+GmxVirtualTileLayer.prototype.initFromDescription = function(layerDescription) {
+    var props = layerDescription.properties,
+        urlTemplate = props.MetaProperties['url-template'].Value,
+        isMercator = !!props.MetaProperties['merc-projection'];
+    
+    var layer = (isMercator ? L.tileLayer.Mercator : L.tileLayer)(urlTemplate);
+    
+    layer.getGmxProperties = function() {
+        return props;
+    }
+
+    return layer;
+}
+
+L.gmx.addLayerClass('TiledRaster', GmxVirtualTileLayer);
+
+})();
