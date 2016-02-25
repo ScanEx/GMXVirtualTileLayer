4b38877501981f29320b4616551ffad88a92cf37
Alexander Parshin <parshin.alexander@gmail.com>
2016-02-25 18:34:17 +0300

    GmxVirtualTileLayer: add WMS virtual layer

--- GmxVirtualTileLayer.js
+++ GmxVirtualTileLayer.js
@@ -30,4 +30,33 @@ L.gmx.addLayerClass('TMS', GmxVirtualTileLayer);
 //depricated - use "TMS" instead
 L.gmx.addLayerClass('TiledRaster', GmxVirtualTileLayer);
 
+var GmxVirtualWMSLayer = function(options) {}
+
+GmxVirtualWMSLayer.prototype.initFromDescription = function(layerDescription) {
+    var WMS_OPTIONS = ['layers', 'styles', 'format', 'transparent', 'version'];
+    var props = layerDescription.properties,
+        baseURL = props.MetaProperties['base-url'].Value,
+        options = {};
+
+    if (props.Copyright) {
+        options.attribution = props.Copyright;
+    }
+    
+    for (var p in props.MetaProperties) {
+        if (WMS_OPTIONS.indexOf(p) !== -1) {
+            options[p] = props.MetaProperties[p].Value;
+        }
+    }
+
+    var layer = L.tileLayer.wms(baseURL, options);
+
+    layer.getGmxProperties = function() {
+        return props;
+    }
+
+    return layer;
+}
+
+L.gmx.addLayerClass('WMS', GmxVirtualWMSLayer);
+
 })();
