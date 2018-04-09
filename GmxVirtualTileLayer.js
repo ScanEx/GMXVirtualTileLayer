61cd0f9449d5301f86580ddc8308ed837c5db5c9
OriginalSin <saleks@scanex.ru>
2018-04-09 13:12:49 +0300

    add Bing layer

--- GmxVirtualTileLayer.js
+++ GmxVirtualTileLayer.js
@@ -161,135 +161,167 @@ GmxVirtualWMSLayer.prototype.initFromDescription = function(layerDescription) {
 
 L.gmx.addLayerClass('WMS', GmxVirtualWMSLayer);
 
+// if (L.TileLayer.MVTSource) {
+	var GmxPBXTileLayer = function(/*options*/) {}
 
-var GmxPBXTileLayer = function(/*options*/) {}
+	GmxPBXTileLayer.prototype.initFromDescription = function(layerDescription) {
+		var props = layerDescription.properties,
+			meta = props.MetaProperties,
+			urlTemplate = meta['url-template'] && meta['url-template'].Value,
+			// isMercator = !!meta['merc-projection'],
+			options = {},
+			mvtOptions = {};
 
-GmxPBXTileLayer.prototype.initFromDescription = function(layerDescription) {
-    var props = layerDescription.properties,
-        meta = props.MetaProperties,
-        urlTemplate = meta['url-template'] && meta['url-template'].Value,
-        // isMercator = !!meta['merc-projection'],
-        options = {},
-		mvtOptions = {};
+		if (!urlTemplate) {
+			return new L.gmx.DummyLayer(props);
+		}
 
-    if (!urlTemplate) {
-        return new L.gmx.DummyLayer(props);
-    }
+		if (props.Copyright) {
+			options.attribution = props.Copyright;
+		}
 
-    if (props.Copyright) {
-        options.attribution = props.Copyright;
-    }
+		if (meta.minZoom) {
+			options.minZoom = meta.minZoom.Value;
+		}
 
-    if (meta.minZoom) {
-        options.minZoom = meta.minZoom.Value;
-    }
+		if (meta.maxZoom) {
+			options.maxZoom = meta.maxZoom.Value;
+		}
 
-    if (meta.maxZoom) {
-        options.maxZoom = meta.maxZoom.Value;
-    }
-
-	mvtOptions.url = urlTemplate;
-	if (meta.debug) {
-		mvtOptions.debug = meta.debug.Value === 'true';
-    }
-	if (meta.visibleLayers) {
-		mvtOptions.visibleLayers = meta.visibleLayers.Value.split(',');
-    }
-	if (meta.clickableLayers) {
-		mvtOptions.clickableLayers = meta.clickableLayers.Value.split(',');
-    }
-	if (meta.filter) {
-	  /**
-	   * The filter function gets called when iterating though each vector tile feature (vtf). You have access
-	   * to every property associated with a given feature (the feature, and the layer). You can also filter
-	   * based of the context (each tile that the feature is drawn onto).
-	   *
-	   * Returning false skips over the feature and it is not drawn.
-	   *
-	   * @param feature
-	   * @returns {boolean}
-	   */
-		var filterLayers = meta.filter.Value.split(',');
-		mvtOptions.filter = function(feature) {
-			for (var i = 0, len = filterLayers.length; i < len; i++) {
-				if (feature.layer.name === filterLayers[i]) {
-				  return true;
+		mvtOptions.url = urlTemplate;
+		if (meta.debug) {
+			mvtOptions.debug = meta.debug.Value === 'true';
+		}
+		if (meta.visibleLayers) {
+			mvtOptions.visibleLayers = meta.visibleLayers.Value.split(',');
+		}
+		if (meta.clickableLayers) {
+			mvtOptions.clickableLayers = meta.clickableLayers.Value.split(',');
+		}
+		if (meta.filter) {
+		  /**
+		   * The filter function gets called when iterating though each vector tile feature (vtf). You have access
+		   * to every property associated with a given feature (the feature, and the layer). You can also filter
+		   * based of the context (each tile that the feature is drawn onto).
+		   *
+		   * Returning false skips over the feature and it is not drawn.
+		   *
+		   * @param feature
+		   * @returns {boolean}
+		   */
+			var filterLayers = meta.filter.Value.split(',');
+			mvtOptions.filter = function(feature) {
+				for (var i = 0, len = filterLayers.length; i < len; i++) {
+					if (feature.layer.name === filterLayers[i]) {
+					  return true;
+					}
 				}
-			}
-			return false;
-		};
-    }
-	var styleTypes = {
-		1: {	//'Point'
-			color: 'rgba(49,79,79,1)',
-			radius: 5,
-			selected: {
-			  color: 'rgba(255,255,0,0.5)',
-			  radius: 6
-			}
-		},
-		2: {	//'LineString'
-			color: 'rgba(161,217,155,0.8)',
-			size: 3,
-			selected: {
-			  color: 'rgba(255,25,0,0.5)',
-			  size: 4
-			}
-		},
-		3: {	//'Polygon'
-			color: 'rgba(149,139,255,0.4)',
-			outline: {
-			  color: 'rgb(20,20,20)',
-			  size: 1
+				return false;
+			};
+		}
+		var styleTypes = {
+			1: {	//'Point'
+				color: 'rgba(49,79,79,1)',
+				radius: 5,
+				selected: {
+				  color: 'rgba(255,255,0,0.5)',
+				  radius: 6
+				}
 			},
-			selected: {
-			  color: 'rgba(255,140,0,0.3)',
+			2: {	//'LineString'
+				color: 'rgba(161,217,155,0.8)',
+				size: 3,
+				selected: {
+				  color: 'rgba(255,25,0,0.5)',
+				  size: 4
+				}
+			},
+			3: {	//'Polygon'
+				color: 'rgba(149,139,255,0.4)',
 				outline: {
-				  color: 'rgba(255,140,0,1)',
-				  size: 2
+				  color: 'rgb(20,20,20)',
+				  size: 1
+				},
+				selected: {
+				  color: 'rgba(255,140,0,0.3)',
+					outline: {
+					  color: 'rgba(255,140,0,1)',
+					  size: 2
+					}
 				}
 			}
+		};
+		if (meta.style) {
+			styleTypes = JSON.parse(meta.style.Value);
 		}
-	};
-	if (meta.style) {
-		styleTypes = JSON.parse(meta.style.Value);
+		mvtOptions.style = function (feature) {
+			var type = feature.type;
+			// var name = feature.layer.name;
+			return styleTypes[type] || {};
+		}
+
+		var mvtSource = new L.TileLayer.MVTSource(Object.assign(mvtOptions, {
+		  getIDForLayerFeature: function(feature) {
+			return feature._id;
+		  },
+
+		  /**
+		   * When we want to link events between layers, like clicking on a label and a
+		   * corresponding polygon freature, this will return the corresponding mapping
+		   * between layers. This provides knowledge of which other feature a given feature
+		   * is linked to.
+		   *
+		   * @param layerName  the layer we want to know the linked layer from
+		   * @returns {string} returns corresponding linked layer
+		   */
+		  layerLink: function(layerName) {
+			if (layerName.indexOf('_label') > -1) {
+			  return layerName.replace('_label','');
+			}
+			return layerName + '_label';
+		  }
+
+		}));
+
+		mvtSource.getGmxProperties = function() {
+			return props;
+		}
+
+		return mvtSource;
 	}
-	mvtOptions.style = function (feature) {
-		var type = feature.type;
-		// var name = feature.layer.name;
-		return styleTypes[type] || {};
-    }
 
-	var mvtSource = new L.TileLayer.MVTSource(Object.assign(mvtOptions, {
-	  getIDForLayerFeature: function(feature) {
-		return feature._id;
-	  },
+	L.gmx.addLayerClass('PBX', GmxPBXTileLayer);
+// }
 
-	  /**
-	   * When we want to link events between layers, like clicking on a label and a
-	   * corresponding polygon freature, this will return the corresponding mapping
-	   * between layers. This provides knowledge of which other feature a given feature
-	   * is linked to.
-	   *
-	   * @param layerName  the layer we want to know the linked layer from
-	   * @returns {string} returns corresponding linked layer
-	   */
-	  layerLink: function(layerName) {
-		if (layerName.indexOf('_label') > -1) {
-		  return layerName.replace('_label','');
+// if (L.tileLayer.bing) {
+	var GmxBingLayer = function(/*options*/) {}
+
+	GmxBingLayer.prototype.initFromDescription = function(layerDescription) {
+		var props = layerDescription.properties,
+			meta = props.MetaProperties,
+			bingMapsKey = meta['bingMapsKey'] && meta['bingMapsKey'].Value,
+			imagerySet = meta['imagerySet'] && meta['imagerySet'].Value,
+			culture = meta['culture'] && meta['culture'].Value,
+			style = meta['style'] && meta['style'].Value,
+			options = {
+				bingMapsKey: bingMapsKey ||'AuhiCJHlGzhg93IqUH_oCpl_-ZUrIE6SPftlyGYUvr9Amx5nzA-WqGcPquyFZl4L',
+				imagerySet: imagerySet || '',
+				culture: culture || '',
+				style: style || ''
+			};
+
+		if (props.Copyright) { options.attribution = props.Copyright; }
+
+		var layer = L.tileLayer.bing(options);
+
+		layer.getGmxProperties = function() {
+			return props;
 		}
-		return layerName + '_label';
-	  }
 
-	}));
+		return layer;
+	}
 
-    mvtSource.getGmxProperties = function() {
-        return props;
-    }
-
-    return mvtSource;
-}
-
-L.gmx.addLayerClass('PBX', GmxPBXTileLayer);
+	L.gmx.addLayerClass('BING', GmxBingLayer);
+// }
 
 })();
